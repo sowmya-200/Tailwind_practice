25c36ffc1f810d80986724fe4a4de57ae9fc402b
unknown <sowma1222chityala@gmail.com>
2023-08-17 07:19:12 +0530

    account_preferences

---
+++ src/app/account-preferences/account-preferences.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatRadioButton} from '@angular/material/radio'
+
+@Component({
+  selector: 'app-account-preferences',
+  templateUrl: './account-preferences.component.html',
+  styleUrls: ['./account-preferences.component.scss']
+})
+export class AccountPreferencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
